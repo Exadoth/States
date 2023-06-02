bb64f3278c2edfabee57e2bf511dcd0ad17313b8
Exadoth <uniomurder@hotmail.com>
2023-06-02 12:56:25 +0300

    Learning States

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import Course from './Course'
+import { useState } from 'react';
+
+
+function getRandomCourse(){
+  const courseArray = ['Angular','Bootstrap','Ccsharp','KompleWeb']
+  return courseArray[Math.floor(Math.random()*courseArray.length)]
+}
+
+function App() {
+
+  const [courses, setcourses] = useState([])
+
+  const handleClick = () => {
+    setcourses([...courses,getRandomCourse()])
+  }
+  const courseList = courses.map((course,index)=>{
+    return <Course key={index} courseName={course}/>
+  });
+  return (
+    <div className="App">
+      <button className='appButton' onClick={handleClick}>Kurs Ekle</button>
+      <div className='courseList'>
+        {courseList}
+      </div>
+      
+    </div>
+  )
+}
+
+export default App;
+
